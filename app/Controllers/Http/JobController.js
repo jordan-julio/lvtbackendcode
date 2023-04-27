37cae5decd473758ed945530d72fdcd887097d9a
z5376268 <z5376268>
2023-04-27 22:42:00 +1000

    try

--- app/Controllers/Http/JobController.js
+++ app/Controllers/Http/JobController.js
@@ -40,7 +40,10 @@ class JobController {
       'budget',
       'user_id'
     ])
-  
+    const user = await auth.getUser()
+    if (user) {
+      data.user_id = user.id;
+    }
     // Upload thumbnail image  
     // Convert the base64 thumbnail to buffer and store in Google Cloud Storage
     if (data.thumbnail !== undefined) {
